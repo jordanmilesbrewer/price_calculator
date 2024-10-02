789ecce05b3409b545a7e89fe501481eb4cd5c07
jordan_brewer <jordanbrewer1994@gmail.com>
2024-10-02 14:54:02 +0100

    Layout has been chaged to use slider in replacement of radio buttons
    radio buttons have been removed
    slider has been put in place and able o show values for price and monthly
    paymnet plan

--- script.js
+++ script.js
@@ -9,10 +9,31 @@
 const selectForm = document.querySelector("#form");
 const subscriptionPrice = document.querySelector("#subscription-price");
 const paymentPeriod = document.querySelector("#check");
+const slider = document.querySelector("#slider");
 
-selectForm.addEventListener("change", (e) => {
-  subscriptionPrice.textContent = `${e.target.value}`;
-  if (paymentPeriod.checked) {
-    subscriptionPrice.textContent = `${e.target.value * 2}`;
+// selectForm.addEventListener("change", (e) => {
+//   subscriptionPrice.textContent = `$${e.target.value}.00 Per Month`;
+//   if (paymentPeriod.checked) {
+//     subscriptionPrice.textContent = `$${
+//       e.target.value * 0.75 * 12
+//     }.00 Per Year`;
+//   }
+// });
+
+function displayPrice(value) {
+  subscriptionPrice.textContent = `$${value}.00`;
+}
+
+slider.addEventListener("change", (e) => {
+  if (slider.value == 0) {
+    displayPrice(8);
+  } else if (slider.value == 25) {
+    displayPrice(12);
+  } else if (slider.value == 50) {
+    displayPrice(16);
+  } else if (slider.value == 75) {
+    displayPrice(24);
+  } else if (slider.value == 100) {
+    displayPrice(36);
   }
 });
