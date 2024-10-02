8068ed1238ce029d96c90801c0a2ad10e7c78875
jordan_brewer <jordanbrewer1994@gmail.com>
2024-10-02 10:49:53 +0100

    Radio buttons selected from script
    Selected all radio buttons in script.js with query selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const tenBtn = document.querySelector("#10k");
+const fiftyBtn = document.querySelector("#50k");
+const OneHundredBtn = document.querySelector("#100k");
+const fiveHundredBtn = document.querySelector("#500k");
+const oneMilBtn = document.querySelector("#1mil");
