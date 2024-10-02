bb09bd452dd59627714e9710eb54cdec8068b095
jordan_brewer <jordanbrewer1994@gmail.com>
2024-10-02 11:36:47 +0100

    Event Listener added
    added an event listener to the form
    the event currently displays the value of the radio button that has been
    selected in the console

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 "use strict";
 
-const tenBtn = document.querySelector("#10k");
-const fiftyBtn = document.querySelector("#50k");
-const OneHundredBtn = document.querySelector("#100k");
-const fiveHundredBtn = document.querySelector("#500k");
-const oneMilBtn = document.querySelector("#1mil");
+// const tenBtn = document.querySelector("#10k");
+// const fiftyBtn = document.querySelector("#50k");
+// const OneHundredBtn = document.querySelector("#100k");
+// const fiveHundredBtn = document.querySelector("#500k");
+// const oneMilBtn = document.querySelector("#1mil");
+
+const form = document.querySelector("#form");
+
+form.addEventListener("change", (e) => {
+  console.log(e.target.value);
+});
