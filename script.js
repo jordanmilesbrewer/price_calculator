b424ea25c468506187a4a7e7fed810d0f258c451
jordan_brewer <jordanbrewer1994@gmail.com>
2024-10-03 16:11:08 +0100

    changed everyhting
    foundout a better more logical way to write the code
    started of by changing the data structor to mke it easier to read and
    easuier to work with
    the principle and function of the calculator remains the same

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 "use strict";
 
-const selectForm = document.querySelector("#form");
-let subscriptionPrice = document.querySelector("#subscription-price");
-const paymentPeriod = document.querySelector("#check");
-const slider = document.querySelector("#slider");
-const views = document.querySelector("#views");
+let pageViews = document.querySelector("#pageviews");
+let pirce = document.querySelector("#price");
+let slider = document.querySelector("#slider");
+let checkbox = document.querySelector("#checkbox");
 
-function displayPrice(value) {
-  if (!paymentPeriod.checked) {
-    subscriptionPrice.textContent = `$${value}.00 / month`;
+let views = [
+  {
+    view: "10k",
+    price: 8,
+  },
+  {
+    view: "50k",
+    price: 12,
+  },
+  {
+    view: "100k",
+    price: 16,
+  },
+  {
+    view: "500k",
+    price: 24,
+  },
+  {
+    view: "1 mil",
+    price: 36,
+  },
+];
+
+slider.addEventListener("change", displayPrice);
+checkbox.addEventListener("change", displayPrice);
+
+function displayPrice() {
+  if (checkbox.checked) {
+    pageViews.innerHTML = views[slider.value].view;
+    price.innerHTML = "$" + views[slider.value].price * 0.75 * 12;
   } else {
-    let newSubPrice = value * 12 * 0.75;
-    subscriptionPrice.textContent = `$${newSubPrice}.00 / year`;
+    pageViews.innerHTML = views[slider.value].view;
+    price.innerHTML = "$" + views[slider.value].price;
   }
 }
-
-let monthlyRate = 36;
-
-slider.addEventListener("change", (e) => {
-  if (slider.value == 0) {
-    monthlyRate = 8;
-    views.textContent = "10k views per month";
-  } else if (slider.value == 25) {
-    monthlyRate = 12;
-    views.textContent = "50k views per month";
-  } else if (slider.value == 50) {
-    monthlyRate = 16;
-    views.textContent = "100k views per month";
-  } else if (slider.value == 75) {
-    monthlyRate = 24;
-    views.textContent = "500k views per month";
-  } else if (slider.value == 100) {
-    monthlyRate = 36;
-    views.textContent = "1 Million views per month";
-  }
-  displayPrice(monthlyRate);
-});
-
-paymentPeriod.addEventListener("change", (e) => {
-  displayPrice(monthlyRate);
-});
