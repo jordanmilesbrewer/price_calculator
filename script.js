45ce218bd7c8fcf5697c4f223954c77f9f0d5537
jordan_brewer <jordanbrewer1994@gmail.com>
2024-10-06 16:40:11 +0100

    Styling finished for price calculator
    styling has been complete and calculator has been check on multpiple
    browsers
    still need to make it responsive for mobile

--- script.js
+++ script.js
@@ -4,26 +4,27 @@ let pageViews = document.querySelector("#pageviews");
 let pirce = document.querySelector("#price");
 let slider = document.querySelector("#slider");
 let checkbox = document.querySelector("#checkbox");
+let paymentPeriod = document.querySelector(".payment-period");
 
 let views = [
   {
-    view: "10k",
+    view: "10k PAGEVIEWS /month",
     price: 8,
   },
   {
-    view: "50k",
+    view: "50k PAGEVIEWS /month",
     price: 12,
   },
   {
-    view: "100k",
+    view: "100k PAGEVIEWS /month",
     price: 16,
   },
   {
-    view: "500k",
+    view: "500k PAGEVIEWS /month",
     price: 24,
   },
   {
-    view: "1 mil",
+    view: "1 MIL PAGEVIEWS /month",
     price: 36,
   },
 ];
@@ -35,8 +36,10 @@ function displayPrice() {
   if (checkbox.checked) {
     pageViews.innerHTML = views[slider.value].view;
     price.innerHTML = "$" + views[slider.value].price * 0.75 * 12;
+    paymentPeriod.textContent = "/year";
   } else {
     pageViews.innerHTML = views[slider.value].view;
     price.innerHTML = "$" + views[slider.value].price;
+    paymentPeriod.textContent = "/month";
   }
 }
